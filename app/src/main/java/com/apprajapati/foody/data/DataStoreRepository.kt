999d563f0b8ac85e5ad0f17fdcb9afaed339749b
apprajapati <apprajapati@hotmail.com>
2023-05-21 17:57:48 -0400

    DataStore sharedpreference repository added

---
+++ app/src/main/java/com/apprajapati/foody/data/DataStoreRepository.kt
@@ -0,0 +1,79 @@
+package com.apprajapati.foody.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.IOException
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.apprajapati.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.apprajapati.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.apprajapati.foody.util.Constants.Companion.PREFERENCES_DIET_TYPE
+import com.apprajapati.foody.util.Constants.Companion.PREFERENCES_DIET_TYPE_ID
+import com.apprajapati.foody.util.Constants.Companion.PREFERENCES_MEAL_TYPE
+import com.apprajapati.foody.util.Constants.Companion.PREFERENCES_MEAL_TYPE_ID
+import com.apprajapati.foody.util.Constants.Companion.PREFERENCES_NAME
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(PREFERENCES_NAME)
+
+@ActivityRetainedScoped
+class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
+
+    private object PreferenceKeys {
+        val selectedMealType = stringPreferencesKey(PREFERENCES_MEAL_TYPE)
+        val selectedMealTypeId = intPreferencesKey(PREFERENCES_MEAL_TYPE_ID)
+
+        val selectedDietType = stringPreferencesKey(PREFERENCES_DIET_TYPE)
+        val selectedDietTypeId = intPreferencesKey(PREFERENCES_DIET_TYPE_ID)
+    }
+
+    private val dataStore: DataStore<Preferences> = context.dataStore
+
+
+    suspend fun saveMealAndDietType(
+        mealType: String,
+        mealTypeId: Int,
+        dietType: String,
+        dietTypeId: Int
+    ) {
+        dataStore.edit { preferences ->
+            preferences[PreferenceKeys.selectedMealType] = mealType
+            preferences[PreferenceKeys.selectedMealTypeId] = mealTypeId
+            preferences[PreferenceKeys.selectedDietType] = dietType
+            preferences[PreferenceKeys.selectedDietTypeId] = dietTypeId
+        }
+    }
+
+    val readMealAndDietType: Flow<MealAndDietType> = dataStore.data.catch { exception ->
+        if (exception is IOException) {
+            emit(emptyPreferences())
+        } else {
+            throw exception
+        }
+    }.map { preferences ->
+        val selectedMealType = preferences[PreferenceKeys.selectedMealType] ?: DEFAULT_MEAL_TYPE
+        val selectedMealTypeId = preferences[PreferenceKeys.selectedMealTypeId] ?: 0
+
+        val selectedDietType = preferences[PreferenceKeys.selectedDietType] ?: DEFAULT_DIET_TYPE
+        val selectedDietTypeId = preferences[PreferenceKeys.selectedDietTypeId] ?: 0
+        MealAndDietType(selectedMealType, selectedMealTypeId, selectedDietType, selectedDietTypeId)
+    }
+
+}
+
+data class MealAndDietType(
+    val selectedMealType: String,
+    val selectedMealTypeId: Int,
+    val selectedDietType: String,
+    val selectedDietTypeId: Int
+)
--- app/src/main/java/com/apprajapati/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/apprajapati/foody/viewmodels/RecipesViewModel.kt
@@ -3,6 +3,9 @@ package com.apprajapati.foody.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.apprajapati.foody.util.Constants.Companion.API_KEY
+import com.apprajapati.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.apprajapati.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.apprajapati.foody.util.Constants.Companion.DEFAULT_RECIPE_NUMBER
 import com.apprajapati.foody.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.apprajapati.foody.util.Constants.Companion.QUERY_DIET
 import com.apprajapati.foody.util.Constants.Companion.QUERY_FILL_INGREDIENTS
@@ -16,10 +19,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPE_NUMBER
         queries[QUERY_KEY] = API_KEY
-        queries[QUERY_TYPE] = "main course"
-        queries[QUERY_DIET] = "gluten free"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
