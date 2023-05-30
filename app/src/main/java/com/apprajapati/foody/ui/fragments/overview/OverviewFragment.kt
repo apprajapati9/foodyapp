b19685fae2ce5a0f4a24d53f4c3a86ee76dfff66
apprajapati <apprajapati@hotmail.com>
2023-05-29 21:54:24 -0400

    populating data from recipe fragment to detailsactivity using safe args, parsehtml custom adapter added.

--- app/src/main/java/com/apprajapati/foody/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/apprajapati/foody/ui/fragments/overview/OverviewFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import coil.load
+import com.apprajapati.foody.R
 import com.apprajapati.foody.databinding.FragmentOverviewBinding
+import com.apprajapati.foody.models.Result
+import org.jsoup.Jsoup
 
 class OverviewFragment : Fragment() {
 
@@ -23,8 +29,121 @@ class OverviewFragment : Fragment() {
     ): View {
 
         _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+
+
+        populateData()
+
+
         return binding.root
     }
+
+    private fun populateData() {
+        val args = arguments
+        val myBundle: Result? =
+            args?.getParcelable("recipeBundle") //Todo: Deprecated. Replace/Remove
+
+        binding.imageViewRecipe.load(myBundle?.image)
+        binding.textViewTitle.text = myBundle?.title
+        binding.textViewLikes.text = myBundle?.aggregateLikes.toString()
+        binding.textViewTime.text = myBundle?.readyInMinutes.toString()
+        myBundle?.summary.let {
+            val summary = Jsoup.parse(it).text()
+            binding.textViewSummary.text = summary //TODO: check if we can load in webview.
+        }
+
+
+        if (myBundle?.vegetarian == true) {
+            binding.imageViewVegetarian.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewVegetarian.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.vegan == true) {
+            binding.imageViewVegan.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewVegan.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.glutenFree == true) {
+            binding.imageViewGlutenFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewGlutenFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.dairyFree == true) {
+            binding.imageViewDairyFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewDairyFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.veryHealthy == true) {
+            binding.imageViewHealthy.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewHealthy.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle?.cheap == true) {
+            binding.imageViewCheap.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.textViewCheap.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
